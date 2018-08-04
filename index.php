94d85c3d34f2538e73a2beaccd1c6c4bd6ab0e0b
Sagar Panchal <sagar.pratham.panchal@gmail.com>
2018-08-04 09:57:17 +0530

    First push

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "sdfsdnfsdfnksdf"; 
+$test = "value can be here";
+
+$i = 2; $j=5;
+
+$c = $i+$j;
+
+include("PHPecho.php");
+
+echo $test;
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
